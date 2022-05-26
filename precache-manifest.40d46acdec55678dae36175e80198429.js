a3588b9642a88f568661d81511b69735922b98d6
Dan Mann <damann1072@gmail.com>
2022-05-26 18:12:34 -0500

    Updates

---
+++ precache-manifest.40d46acdec55678dae36175e80198429.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89470bf4d2f2f44d3a2610de348a19a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "365d21668dc570c47754",
+    "url": "/static/css/2.7cc9718e.chunk.css"
+  },
+  {
+    "revision": "ce4844d45bc7b6e25afc",
+    "url": "/static/css/main.d83b54a8.chunk.css"
+  },
+  {
+    "revision": "365d21668dc570c47754",
+    "url": "/static/js/2.1f25d8cb.chunk.js"
+  },
+  {
+    "revision": "f6ce9b198908e234c073cb4697347be2",
+    "url": "/static/js/2.1f25d8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4844d45bc7b6e25afc",
+    "url": "/static/js/main.98859af4.chunk.js"
+  },
+  {
+    "revision": "c5bb459f079dc2a36d65",
+    "url": "/static/js/runtime-main.cf9698b9.js"
+  }
+]);
